b4f99a129d939b56ed764d9d4a71595b778259fe
impliedfeline <kinnunen.jussi@hotmail.com>
2023-01-31 00:14:15 +0200

    Improve config error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::Context;
 use helsinki_bike_app::{config::Settings, fetch_and_parse};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config = Settings::new()?;
+    let config = Settings::new().context("Failed to read config")?;
 
     for url in config.journey_data_urls {
         fetch_and_parse(url).await?;
